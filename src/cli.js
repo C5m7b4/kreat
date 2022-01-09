e306fe3ec32d3893579df4c5b0b2acf0f6867a18
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-08 20:55:56 -0600

    remove typescript library

--- src/cli.js
+++ src/cli.js
@@ -45,7 +45,6 @@ async function promptForMissingOptions(options) {
         'typescript-react',
         'javascript-playground',
         'typescript-playground',
-        'typescript-library',
       ],
       default: defaultTemplate,
     });
